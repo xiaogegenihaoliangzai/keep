f449d46329b748a8e797f0c9a11297f6977e8fb5
Hitty-H <a3323@foxmail.com>
2019-07-23 18:33:03 +0800

    Hitty-H Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import listAll from '../../pages/listAll'
-
+import HelloWorld from '@/components/HelloWorld';
+import listAll from '@/pages/listAll';
+import community from '@/pages/community';
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +15,11 @@ export default new Router({
       path: 'listall',
       name: 'listAll',
       component: listAll
+    },
+    {
+      path: 'community',
+      name: 'community',
+      component: community
     }
   ]
 })
