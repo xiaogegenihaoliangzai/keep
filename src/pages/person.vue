8d538d4e6b548d38ce94c055af9d1ab07a9bada7
hello-123456 <635034242@qq.com>
2019-07-29 22:32:29 +0800

    7777

---
+++ src/pages/person.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+	  aaaaa
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+	methods:{
+	}
+}
+</script>
+<style scoped>
+
+</style>
