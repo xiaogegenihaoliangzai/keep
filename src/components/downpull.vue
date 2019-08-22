5fab510ca0f2bdbc73d1f575202e986a5652a422
Hitty-H <a3323@foxmail.com>
2019-08-22 21:05:44 +0800

    hitty-h 8.22更新

--- src/components/downpull.vue
+++ src/components/downpull.vue
@@ -1,10 +1,8 @@
 <template>
     <yd-pullrefresh :callback="loadList" ref="pullrefreshDemo">
-
         <yd-list theme="4">
-    <comcont></comcont>
+           <comcont></comcont>
         </yd-list>
-
     </yd-pullrefresh>
 </template>
 
@@ -20,25 +18,16 @@
             loadList() {
             	console.log(999)
                 const url = 'http://list.ydui.org/getdata.php';
-
                 this.$http.jsonp(url, {params: {type: 'pulldown', page: this.page}}).then((response) => {
-
                     const _list = response.body;
-
-                    this.$dialog.toast({
-                        mes: _list.length > 0 ? '为您更新了' + _list.length + '条内容' : '已是最新内容'
-                    });
-
+            
                     this.$refs.pullrefreshDemo.$emit('ydui.pullrefresh.finishLoad');
-
                     this.page++;
                 });
             }
         },
-        		components: {
-		
+        components: {
 			comcont
-			
 		}
     }
 </script>
